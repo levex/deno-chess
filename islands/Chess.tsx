34920cf4b7de3656cf8a56652d4f3db5b3bcc723
Levente Kurusa <lkurusa@kernelstuff.org>
2023-05-01 15:09:49 +0200

    wtf

--- islands/Chess.tsx
+++ islands/Chess.tsx
@@ -1,17 +1,31 @@
-import { useState } from "preact/hooks";
+import { useState, useEffect } from "preact/hooks";
 import Chessboard from "chessboardjsx";
 import { Chess, ShortMove } from "chess.js";
+
+function useBroadcastChannel(channelName, onMessage) {
+  const [channel, setChannel] = useState(null);
+
+  useEffect(() => {
+    const newChannel = new BroadcastChannel(channelName);
+    newChannel.onmessage = onMessage;
+    setChannel(newChannel);
+
+    return () => {
+      newChannel.close();
+    };
+  }, [channelName, onMessage]);
+
+  return channel;
+}
 
 export default function ChessIsland(props) {
   const [chess, setChess] = useState(new Chess(props.fen));
   const [fen, setFen] = useState(chess.fen());
-
-  const gameChannel = new BroadcastChannel(`game-${props.gameUuid}`);
-  gameChannel.onmessage = (msg) => {
+  const gameChannel = useBroadcastChannel(`game-${props.gameUuid}`, (msg) => {
     const newFen = msg.data;
     setFen(newFen);
     setChess(new Chess(newFen));
-  };
+  });
 
   const myMove = (chess: Chess): boolean => {
     let currentTurn = chess.turn();
